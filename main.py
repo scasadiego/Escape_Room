e248e9f53629ade1a329defa320c74bb5f496209
Simon <scasadiego@unal.edu.co>
2026-03-12 00:10:43 +0000

    fix: corregidos bugs en la infraestructura

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ def hello_world():
     return '<h1>¡Felicidades! Has escapado del cuarto de DevOps. 🚀</h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
